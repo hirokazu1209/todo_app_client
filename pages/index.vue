d5f5e735651824cd54257f26ac9dd7b03d2ffbc0
hirokazu <n.hirokazu92@gmail.com>
2022-03-24 23:02:19 +0900

    indexファイルの実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-left">
-        TODOリスト
-      </v-card>
-    </v-col>
-  </v-row>
+  <section class="container">
+    <h1>Todoリスト</h1>
+    <div class="addArea">
+      <input type="text" name="addName" id ="addName" placeholder="タスクを入力してください">
+      <button id="addButton" class="button button--green">追加</button>
+    </div>
+    <div class="Filter">
+      <button class="button button--gray is-active">全て</button>
+      <button class="button button--gray">作業前</button>
+      <button class="button button--gray">作業中</button>
+      <button class="button button--gray">完了</button>
+    </div>
+    <table class="Lists">
+      <thead>
+        <tr>
+          <th>タスク</th>
+          <th>登録日時</th>
+          <th>状態</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>テスト</td>
+          <td>2020-04-30 17:00</td>
+          <td><button class="button button--yet">作業前</button></td>
+          <td><button class="button button--delete">削除</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
@@ -13,3 +37,6 @@ export default {
   name: 'IndexPage',
 }
 </script>
+<style>
+
+</style>
