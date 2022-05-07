82270964bd61c5bf88454900ea7f72fb1c68d002
hirokazu <n.hirokazu92@gmail.com>
2022-05-07 23:41:59 +0900

    コンポーネント化_セレクトフィールド

---
+++ components/molecules/MSelectForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-select
+    :items="items"
+    label="テスト"
+  ></v-select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  data: () => ({
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+  }),
+})
+</script>
+<style>
+</style>
+
