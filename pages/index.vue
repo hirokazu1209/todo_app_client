cff4ce13eab1cb6cdf259427552e588437078a60
hirokazu <n.hirokazu92@gmail.com>
2022-05-08 21:12:31 +0900

    入力フォーム項目実装

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     />
     <v-container class="grey lighten-5">
       <MTitle
-        :title="applicantDetails"
+        :title="applicantInformation"
       />
       <v-row>
         <v-col cols="6">
@@ -21,6 +21,59 @@
           <MTextField></MTextField>
         </v-col>
       </v-row>
+      <MTitle
+        :title="applicationInformation"
+      />
+      <v-row>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+        <v-col cols="6">
+          <MTextField></MTextField>
+        </v-col>
+        <v-col cols="6">
+          <MTextField></MTextField>
+        </v-col>
+      </v-row>
+      <MTitle
+        :title="recruitmentInformation"
+      />
+      <v-row>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+      </v-row>
+      <MTitle
+        :title="reason"
+      />
+      <v-row>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+      </v-row>
+      <MTitle
+        :title="fileData"
+      />
+      <v-row>
+        <v-col cols="6">
+          <MSelectForm></MSelectForm>
+        </v-col>
+      </v-row>
+      <MTitle
+        :title="comment"
+      />
       <button type="submit" class="btn btn-primary" @click.prevent="submit">送信</button>
     </v-container>
   </div>
@@ -43,11 +96,24 @@ export default defineComponent({
   //     console.log(this.form)
   //   }
   setup(){
-    const mainTitle = ref<string>('応募者新規作成画面');
-    const applicantDetails = ref<string>('応募者情報');
+    const mainTitle = ref<string>('新規応募者登録');
+    const applicantInformation = ref<string>('応募者情報');
+    const applicationInformation = ref<string>('応募情報');
+    const recruitmentInformation = ref<string>('採用情報');
+    const reason = ref<string>('終了理由');
+    const fileData = ref<string>('応募者ファイルデータ');
+    const comment = ref<string>('コメント');
+
+
+
     return {
       mainTitle,
-      applicantDetails,
+      applicantInformation,
+      applicationInformation,
+      recruitmentInformation,
+      reason,
+      fileData,
+      comment
     }
   }
 });
